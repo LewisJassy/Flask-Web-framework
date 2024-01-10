c5f9cb9715590a18941f3266ec2adbe28b105320
LewisJassy <jassylewis70@gmail.com>
2024-01-11 00:40:45 +0300

    updated the code

--- Flask-Appliction/app.py
+++ Flask-Appliction/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 # wsgi is usefull when deploying to the web host
@@ -8,7 +9,12 @@ wsgi_app = app.wsgi_app
 @app.route('/')
 def hello():
     '''Render a sample page'''
-    return "<h1>Hello World!!!</h1>"
+    now = datetime.now()
+    formatted_date = now.strftime("%A, %d, %B, %Y at %X")
+    html_content = "<html><head><title>Hello Flask</title></head><body>"
+    html_content += "<strong>Hello Flask!</strong> on " + formatted_date
+    html_content += "</body></html>"
+    return html_content
 
 
 if __name__ == "__main__":
