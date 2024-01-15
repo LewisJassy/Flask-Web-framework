c72d8be583d84ccbeb4621b3beb1598a6dc663c9
LewisJassy <jassylewis70@gmail.com>
2024-01-16 01:22:33 +0300

    create a database

--- Blog/app.py
+++ Blog/app.py
@@ -1,23 +1,43 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from form import RegstrationForm, LoginForm
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
 app.config["SECRET_KEY"] = 'c6d75c5b3fe288fa1ced520a011e19'
-posts = [{
-    "author": 'Lewis Njaci',
-    " title": "Blog post",
-    "content": 'Blog'
-},
-    {
-        "None",
-    }
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False, unique=True)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.password}, {self.image_file}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
