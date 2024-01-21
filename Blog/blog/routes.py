05a0dac868660c5630c5eca0783b9c08ad3bbc0a
LewisJassy <jassylewis70@gmail.com>
2024-01-21 08:31:40 +0300

    update to get image, and remove confirm password field

--- Blog/blog/routes.py
+++ Blog/blog/routes.py
@@ -58,6 +58,6 @@ def account():
         form.username.data = current_user.username
         form.email.data = current_user.email
     
-    image_file = url_for('static', filename='profile_pics/' + current_user.code)
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', form=form, image_file=image_file)
 
