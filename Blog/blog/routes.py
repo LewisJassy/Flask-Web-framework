2898fb11e1ea70da3293cf434368cdf75d3c3f3d
LewisJassy <jassylewis70@gmail.com>
2024-01-21 07:59:26 +0300

    add an profile pic folder and add an image

--- Blog/blog/routes.py
+++ Blog/blog/routes.py
@@ -57,5 +57,7 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    return render_template('account.html', title='Account', form=form)
+    
+    image_file = url_for('static', filename='profile_pics/' + current_user.code)
+    return render_template('account.html', title='Account', form=form, image_file=image_file)
 
