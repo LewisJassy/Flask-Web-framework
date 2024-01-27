917c23bc7ba500b65416edbee38cd55720195d74
LewisJassy <jassylewis70@gmail.com>
2024-01-27 20:28:07 +0300

    code updated

--- Blog/blog/config.py
+++ Blog/blog/config.py
@@ -1,9 +1,12 @@
 import os
+import secrets
 
+# Generate a random 32-byte (256-bit) hexadecimal string
+secret_key = secrets.token_hex(32)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
